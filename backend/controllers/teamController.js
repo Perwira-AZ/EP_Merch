7e28b50fbef09f0103d29bb610aee0c77b5c1a88
giftormdh <giftoramadha@gmail.com>
2023-09-27 02:01:48 +0700

    refactor: add try-catch to team and user controller

--- backend/controllers/teamController.js
+++ backend/controllers/teamController.js
@@ -1,55 +1,81 @@
-const Team = require('../models/teamModel');
-const mongoose = require('mongoose');
+const Team = require("../models/teamModel");
+const mongoose = require("mongoose");
 
 // Get all team
 const getAllTeams = async (req, res) => {
+  try {
     const teams = await Team.find({});
-
     res.status(200).json(teams);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
-// Get a singel team
+// Get a single team
 const getTeam = async (req, res) => {
+  try {
     const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: "Team doesn't exist" });
+      throw new Error("Team doesn't exist");
     }
 
-    const team = await Team.findById(id, 'teamName teamLeader teamLocation teamStart teamEnd teamCompetition teamDescription teamMember');
+    const team = await Team.findById(id, "teamName teamLeader teamLocation teamStart teamEnd teamCompetition teamDescription teamMember");
+
+    if (!team) {
+      throw new Error("Team doesn't exist");
+    }
+
+    res.status(200).json(team);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
 };
 
 // Create new team
 const createTeam = async (req, res) => {
-    const { teamName, teamLeader, teamLocation, teamStart, teamEnd, teamCompetition, teamDescription, teamMember } = req.body;
+  const { teamName, teamLeader, teamLocation, teamStart, teamEnd, teamCompetition, teamDescription, teamMember } = req.body;
 
-    try {
-        const team = await Team.create({ teamName, teamLeader, teamLocation, teamStart, teamEnd, teamCompetition, teamDescription, teamMember });
-        res.status(200).json(user);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+  try {
+    const team = await Team.create({
+      teamName,
+      teamLeader,
+      teamLocation,
+      teamStart,
+      teamEnd,
+      teamCompetition,
+      teamDescription,
+      teamMember,
+    });
+    res.status(200).json(team);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 // Delete a team
 const deleteTeam = async (req, res) => {
+  try {
     const { id } = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: "Team doesn't exist" });
+      throw new Error("Team doesn't exist");
     }
 
     const team = await Team.findOneAndDelete({ _id: id });
 
     if (!team) {
-        return res.status(404).json({ error: "User doesn't exist" });
+      throw new Error("Team doesn't exist");
     }
 
-    res.status(200).json(user);
+    res.status(200).json(team);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
 };
 
 module.exports = {
-    getAllTeams,
-    getTeam,
-    createTeam,
-    deleteTeam,
+  getAllTeams,
+  getTeam,
+  createTeam,
+  deleteTeam,
 };
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,77 +1,89 @@
-const User = require('../models/userModel');
-const mongoose = require('mongoose');
+const User = require("../models/userModel");
+const mongoose = require("mongoose");
 
 // Get a single user
 const getUser = async (req, res) => {
+  try {
     const { id } = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: "User doesn't exist" });
+      throw new Error("User doesn't exist");
     }
 
-    const user = await User.findById(id, 'name userName userEmail team');
+    const user = await User.findById(id, "name userName userEmail team");
 
     if (!user) {
-        return res.status(404).json({ error: "User doesn't exist" });
+      throw new Error("User doesn't exist");
     }
 
     res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
 };
 
 // Create new user
 const createUser = async (req, res) => {
-    const { name, userName, userEmail, password } = req.body;
+  const { name, userName, userEmail, password } = req.body;
 
-    try {
-        const user = await User.create({ name, userName, userEmail, password });
-        res.status(200).json(user);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+  try {
+    const user = await User.create({ name, userName, userEmail, password });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 // Delete a user
 const deleteUser = async (req, res) => {
+  try {
     const { id } = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: "User doesn't exist" });
+      throw new Error("User doesn't exist");
     }
 
     const user = await User.findOneAndDelete({ _id: id });
 
     if (!user) {
-        return res.status(404).json({ error: "User doesn't exist" });
+      throw new Error("User doesn't exist");
     }
 
     res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
 };
 
 // Update a user
 const updateUser = async (req, res) => {
+  try {
     const { id } = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: "User doesn't exist" });
+      throw new Error("User doesn't exist");
     }
 
     const user = await User.findOneAndUpdate(
-        { _id: id },
-        {
-            ...req.body,
-        }
+      { _id: id },
+      {
+        ...req.body,
+      }
     );
 
     if (!user) {
-        return res.status(404).json({ error: "User doesn't exist" });
+      throw new Error("User doesn't exist");
     }
 
     res.status(200).json(User.findById(id));
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
 };
 
 module.exports = {
-    getUser,
-    createUser,
-    deleteUser,
-    updateUser,
+  getUser,
+  createUser,
+  deleteUser,
+  updateUser,
 };
