a3188c0226d14167f197563d531796df65d76283
Perwira.AZ <perwiraakhdanz@gmail.com>
2023-09-28 10:36:06 +0700

    feat: add register and login with encryption and validaion

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -28,5 +30,54 @@ const userSchema = new Schema({
         },
     ],
 });
+
+// static register method
+userSchema.statics.register = async function (name, userName, userEmail, password) {
+    //validation
+    if (!name || !userName || !userEmail || !password) {
+        throw Error('All fields must be filled');
+    }
+    if (!validator.isEmail(userEmail)) {
+        throw Error('Email is not valid');
+    }
+    if (password.length < 8) {
+        throw Error('Password must contains at least 8 characters');
+    }
+
+    const emailExists = await this.findOne({ userEmail });
+    const userNameExists = await this.findOne({ userName });
+
+    if (emailExists) {
+        throw Error('Email already in use');
+    }
+    if (userNameExists) {
+        throw Error('Username already in use');
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    const user = await this.create({ name, userName, userEmail, password: hash });
+
+    return user;
+};
+
+// static login method
+userSchema.statics.login = async function (userEmail, password) {
+    if (!userEmail || !password) {
+        throw Error('All fields must be filled');
+    }
+
+    const user = await this.findOne({ userEmail });
+    if (!user) {
+        throw Error('Email or password is wrong');
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+        throw Error('Email or password is wrong');
+    }
+
+    return user;
+};
 
 module.exports = mongoose.model('User', userSchema);
