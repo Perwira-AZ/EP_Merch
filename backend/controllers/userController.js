19f271eeef69510391f2b5b03b94e584804d6c3d
Perwira.AZ <perwiraakhdanz@gmail.com>
2023-09-28 10:58:06 +0700

    feat: Add createToken function, register and login API. Delete getAllUsers, createNewUser, and deleteUser API.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,70 +1,30 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
-//Get a all users
-const getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ error: err.message() });
-    }
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '1d' });
 };
 
 // Get a single user
 const getUser = async (req, res) => {
     const { id } = req.params;
 
-    try{
+    try {
         if (!mongoose.Types.ObjectId.isValid(id)) {
-            throw new Error ("User doesn't exist");
+            throw new Error("User doesn't exist");
         }
 
         const user = await User.findById(id, 'name userName userEmail team');
 
         if (!user) {
-            return res.status(404).json({ error: "User doesn't exist" });
+            throw new Error("User doesn't exist");
         }
 
-        res.status(200).json(user);
-    } catch (err){
-        res.status(404).json({ error: err.message() });
-    }
-};
-
-// Create new user
-const createUser = async (req, res) => {
-    const { name, userName, userEmail, password } = req.body;
-
-    try {
-        const user = await User.create({ name, userName, userEmail, password });
         res.status(200).json(user);
     } catch (err) {
-        res.status(400).json({ error: err.message() });
-    }
-};
-
-// Delete a user
-const deleteUser = async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            throw new Error("User doesn't exist");
-        }
-    
-        const user = await User.findOneAndDelete({ _id: id });
-    
-        if (!user) {
-            throw new Error("User doesn't exist");
-        }
-    
-        res.status(200).json(user);
-    } catch (error) {
         res.status(404).json({ error: err.message() });
     }
-
 };
 
 // Update a user
@@ -75,31 +35,56 @@ const updateUser = async (req, res) => {
         if (!mongoose.Types.ObjectId.isValid(id)) {
             throw new Error("User doesn't exist");
         }
-    
+
         const user = await User.findOneAndUpdate(
             { _id: id },
             {
                 ...req.body,
             }
         );
-    
+
         const updatedUser = await User.findById(id);
-    
+
         if (!user) {
             throw new Error("User doesn't exist");
         }
-    
+
         res.status(200).json(updatedUser);
     } catch (error) {
         res.status(404).json({ error: err.message() });
     }
+};
 
+// Register user
+const registerUser = async (req, res) => {
+    const { name, userName, userEmail, password } = req.body;
+
+    try {
+        const user = await User.register(name, userName, userEmail, password);
+
+        res.status(200).json({ user });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// Login user
+const loginUser = async (req, res) => {
+    const { userEmail, password } = req.body;
+
+    try {
+        const user = await User.login(userEmail, password);
+        const token = await createToken(user._id);
+
+        res.status(200).json({ token });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 };
 
 module.exports = {
-    getAllUsers,
     getUser,
-    createUser,
-    deleteUser,
     updateUser,
+    registerUser,
+    loginUser,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const User = require('../models/userModel');
-const { getAllUsers, getUser, createUser, deleteUser, updateUser } = require('../controllers/userController');
+const { getUser, updateUser, registerUser, loginUser } = require('../controllers/userController');
 
 const router = express.Router();
-
-// GET all users
-router.get('/', getAllUsers);
 
 // GET a single user
 router.get('/:id', getUser);
 
-// DELETE a user
-router.delete('/:id', deleteUser);
-
 // UPDATE a user
 router.patch('/:id', updateUser);
 
-// POST a new user
-router.post('/', createUser);
+// Register new user
+router.post('/register', registerUser);
+
+// Login a user
+router.post('/login', loginUser);
 
 module.exports = router;
