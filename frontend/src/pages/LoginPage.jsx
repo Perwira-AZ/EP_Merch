c4d2032c5fabfece14c333b62f26f3f8dba5879a
giftormdh <giftoramadha@gmail.com>
2023-11-12 19:18:39 +0700

    style : tidy up the temporary login display

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,42 +1,44 @@
-import React from 'react';
+import React from "react";
 
 function LoginPage({ onLogin }) {
-    const [user, setUser] = React.useState({
-        userEmail: '',
-        password: '',
-    });
+  const [user, setUser] = React.useState({
+    userEmail: "",
+    password: "",
+  });
 
-    function onEmailChange(event) {
-        setUser((prevState) => ({
-            ...prevState,
-            userEmail: event.target.value,
-        }));
-    }
+  function onEmailChange(event) {
+    setUser((prevState) => ({
+      ...prevState,
+      userEmail: event.target.value,
+    }));
+  }
 
-    function onPasswordChange(event) {
-        setUser((prevState) => ({
-            ...prevState,
-            password: event.target.value,
-        }));
-    }
+  function onPasswordChange(event) {
+    setUser((prevState) => ({
+      ...prevState,
+      password: event.target.value,
+    }));
+  }
 
-    function onSubmitHandler(event) {
-        console.log(user);
-        event.preventDefault();
-        onLogin(user);
-    }
+  function onSubmitHandler(event) {
+    console.log(user);
+    event.preventDefault();
+    onLogin(user);
+  }
 
-    return (
-        <div className="login-page">
-            <form className="input-login" onSubmit={onSubmitHandler}>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" onChange={onEmailChange} />
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" onChange={onPasswordChange} />
-                <button>Login</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="login-page-temp">
+      <form className="input-login" onSubmit={onSubmitHandler}>
+        <label>email: testUser1@test.com</label>
+        <label>pw: test1234</label>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" onChange={onEmailChange} />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" onChange={onPasswordChange} />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
