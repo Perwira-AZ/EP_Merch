5ac5f36919a580701a1ad22fa0010092a901aa0a
Perwira.AZ <perwiraakhdanz@gmail.com>
2023-11-12 13:52:14 +0700

    style: use tailwind css for MyTeamPage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,25 +5,25 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <header>
+        <header className="z-[100] bg-blue-500 shadow fixed top-0 left-0 h-[60px] w-full box-border flex flex-row justify-between items-center px-6">
             <div className="header__logo">
-                <img src={TeamUP} alt="" />
+                <img src={TeamUP} alt="" className="h-9" />
             </div>
-            <ul className="header__menu">
-                <Link to="/">
+            <ul className="header__menu flex flex-row justify-center gap-x-5 font-medium list-none">
+                <Link to="/" className="text-white">
                     <li>Home</li>
                 </Link>
-                <Link to="/myteam">
+                <Link to="/myteam" className="text-white">
                     <li>My Team</li>
                 </Link>
-                <Link>
+                <Link className="text-white">
                     <li>Explore Team</li>
                 </Link>
-                <Link>
+                <Link className="text-white">
                     <li>Profile</li>
                 </Link>
             </ul>
-            <div className="header__logout">
+            <div className="text-white text-3xl">
                 <TbLogout />
             </div>
         </header>
--- frontend/src/pages/MyTeamPage.jsx
+++ frontend/src/pages/MyTeamPage.jsx
@@ -20,23 +20,37 @@ function MyTeamPage() {
     }, []);
 
     return (
-        <div className="myTeamPage">
+        <div className="myTeamPage bg-zinc-50 px-15 pb-8">
             <div className="my-team">
-                <div className="created-team">
-                    <div className="created-team__header">
-                        <h2 className="my-team__title">Created Team</h2>
-                        <Link to="/joinrequest">
-                            <button className="my-team__join-request">Join Request</button>
+                <div className="mx-auto min-[1330px]:max-w-7xl min-[900px]:max-w-[840px] max-w-[400px]">
+                    <div className="created-team">
+                        <div className="pt-16 mb-7 flex flex-row items-center justify-between">
+                            <h2 className="text-indigo-950 text-[45px] font-bold leading-[34px]">Created Team</h2>
+                            <Link to="/joinrequest" className="w-48">
+                                <button className="w-full h-12 bg-indigo-950 rounded-xl text-white text-lg">Join Request</button>
+                            </Link>
+                        </div>
+                        {isLoading ? (
+                            <p>Loading...</p>
+                        ) : myTeam != null && myTeam.myTeamCreated.length ? (
+                            <TeamList teams={myTeam.myTeamCreated} />
+                        ) : (
+                            <p>No Team</p>
+                        )}
+                        <Link className="w-full">
+                            <button className="w-full h-12 bg-indigo-950 rounded-xl text-white text-lg">+ Create New Team</button>
                         </Link>
                     </div>
-                    {isLoading ? <p>Loading...</p> : myTeam != null && myTeam.myTeamCreated.length ? <TeamList teams={myTeam.myTeamCreated} /> : <p>No Team</p>}
-                    <Link>
-                        <button className="my-team__create-button">+ Create New Team</button>
-                    </Link>
-                </div>
-                <div className="joined-team">
-                    <h2 className="my-team__title">Joined Team</h2>
-                    {isLoading ? <p>Loading...</p> : myTeam != null && myTeam.myTeamJoined.length ? <TeamList teams={myTeam.myTeamJoined} /> : <p>No Team</p>}
+                    <div className="joined-team">
+                        <h2 className="pt-11 mb-7 text-indigo-950 text-[45px] font-bold leading-[34px]">Joined Team</h2>
+                        {isLoading ? (
+                            <p>Loading...</p>
+                        ) : myTeam != null && myTeam.myTeamJoined.length ? (
+                            <TeamList teams={myTeam.myTeamJoined} />
+                        ) : (
+                            <p>No Team</p>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
