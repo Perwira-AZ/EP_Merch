fcdb4c1584d01b7bfdf68615eeb9b5fe0e0db7f2
Perwira.AZ <perwiraakhdanz@gmail.com>
2023-11-25 18:22:04 +0700

    fix: add function

--- frontend/src/pages/ExploreTeamPage.jsx
+++ frontend/src/pages/ExploreTeamPage.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { getMyTeam } from "../utils/fetch";
-import TeamList from "../components/TeamList";
+import React from 'react';
+import { searchTeam } from '../utils/fetch';
+import TeamList from '../components/TeamList';
 
 function ExploreTeamPage() {
   const [myTeam, setMyTeam] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
-    getMyTeam()
+    searchTeam()
       .then(({ data }) => {
         setMyTeam(data);
         setIsLoading(false); // Set loading to false after data is fetched
       })
       .catch(({ error }) => {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error);
         setIsLoading(false); // Set loading to false in case of an error
       });
   }, []);
@@ -28,12 +28,12 @@ function ExploreTeamPage() {
             <div className="mb-3 xl:w-1/2 mx-auto">
               <input
                 type="search"
-                className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+                className="relative mb-10 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
                 id="exampleSearch"
                 placeholder="Find by Name or Competition"
               />
             </div>
-            {isLoading ? <p>Loading...</p> : myTeam != null && myTeam.myTeamCreated.length ? <TeamList teams={myTeam.myTeamCreated} /> : <p>No Team</p>}
+            {isLoading ? <p>Loading...</p> : myTeam != null && myTeam.length ? <TeamList teams={myTeam} /> : <p>No Team</p>}
           </div>
         </div>
       </div>
