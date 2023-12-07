917a8e61f0a9bc0a4b77b6fef996a919e4655fa5
novaldypratama <novaldypratama@mail.ugm.ac.id>
2023-12-07 20:47:14 +0700

    fix: improve profile components

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -5,7 +5,7 @@ import EditProfile from '../components/EditProfile'; // Import the EditProfile c
 import TeamList from '../components/TeamList';
 import TeamCard from '../components/TeamCard';
 
-function ProfilePage(user) {
+function ProfilePage({user}) {
   const [myTeam, setMyTeam] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -20,56 +20,11 @@ function ProfilePage(user) {
   }, []);
 
   return (
-    <div className="profilePage bg-zinc-50 pt-[60px] pb-8 min-h-screen box-border">
-      <div className="profile">
-        <div className="mx-auto min-[1330px]:max-w-7xl min-[900px]:max-w-[840px] max-w-[400px]">
-        <div className="flex flex-col items-center">
-        <h2 className="text-xl font-semibold text-blue-500 mb-4">Your Profile</h2>
-        <div className="avatar-section mb-4">
-          <img src="path_to_avatar_image" alt="User Avatar" className="rounded-full h-24 w-24 mb-4" />
-          <div className="flex flex-col">
-            {/* <button onClick={handleProfilePictChange} className="bg-blue-500 text-white rounded py-2 px-4 mb-2">Upload your avatar</button>
-            <button onClick={handleProfilePictDelete} className="bg-white text-blue-500 border border-blue-500 rounded py-2 px-4">Delete avatar</button> */}
-          </div>
-          <div className="user-info w-full bg-white p-4 rounded-lg shadow-md">
-          <div className="info-field mb-4">
-            <label htmlFor="full-name" className="block text-sm font-medium text-gray-700">Full Name</label>
-            <input
-              type="text"
-              id="full-name"
-              // value={fullName}
-              // onChange={(e) => setFullName(e.target.value)}
-              placeholder="Full Name"
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>
-          <div className="info-field mb-4">
-            <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
-            <input
-              type="text"
-              id="username"
-              // value={username}
-              // onChange={(e) => setUsername(e.target.value)}
-              placeholder="Username"
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>
-          <div className="info-field mb-4">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-            <input
-              type="email"
-              id="email"
-              // value={email}
-              // onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email"
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>
-          {/* <button onClick={handleSave} className="w-full bg-blue-500 text-white rounded py-2 px-4">Save</button> */}
-        </div>
+    <div className="profilePage bg-zinc-50 pt-[60px] pe-8 min-h-screen box-border">
+      <div className="profile pe-16">
+      <div className="mx-auto min-[330px]:max-w-7xl min-[900px]:max-w-[840px] max-w-[360px]">
+        <EditProfile userId={user._id}/>
       </div>
-    </div>
-    </div>
     </div>
     </div>);
 }
