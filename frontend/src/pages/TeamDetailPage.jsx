6441708aa405e88d543c92d8704bb4c0da13c27b
giftormdh <giftoramadha@gmail.com>
2023-12-07 14:52:59 +0700

    fix: minor styling

--- frontend/src/pages/TeamDetailPage.jsx
+++ frontend/src/pages/TeamDetailPage.jsx
@@ -28,7 +28,7 @@ function TeamDetailPage() {
   } else {
     return (
       <div className="relative bg-white flex flex-row-reverse justify-center w-full px-[30px] gap-5 max-[760px]:flex-col z-0" style={{ overflowX: "hidden" }}>
-        <svg height="256" viewBox="0 0 1920 256" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute z[-1] top-0 w-full">
+        <svg height="256" viewBox="0 0 1920 256" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute z-[-1] top-0 w-full">
           <path d="M0 0V205.024C0 205.024 344.437 319.72 960 205.024C1575.56 90.3293 1920 205.024 1920 205.024V7.53698e-05L0 0Z" fill="url(#paint0_linear_283_1413)" />
           <defs>
             <linearGradient id="paint0_linear_283_1413" x1="960" y1="0" x2="960" y2="449.688" gradientUnits="userSpaceOnUse">
