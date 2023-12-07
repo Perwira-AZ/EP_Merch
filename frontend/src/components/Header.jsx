21b49aa4fea87042eb1fa20fbbad7631ec644e94
Angelicaviera <angelica.callysta.viera.mail.ugm.ac.id>
2023-12-07 16:45:21 +0700

    navigation bar style update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TbLogout } from 'react-icons/tb';
 import { IoNotificationsSharp } from 'react-icons/io5';
 import TeamUP from '../assets/Team.png';
 import { Link } from 'react-router-dom';
 import { logout } from '../utils/fetch';
 
-function Header({ clickNotif, onLogout }) {
+function Header({ clickNotif }) {
+  const [activeMenuItem, setActiveMenuItem] = useState('');
+
+  const handleMenuItemClick = (menuItem) => {
+    setActiveMenuItem(menuItem);
+  };
+
   return (
     <header className="z-[100] bg-blue-500 shadow fixed top-0 left-0 h-[60px] w-full box-border flex flex-row justify-between items-center px-6">
       <div className="header__logo">
         <img src={TeamUP} alt="" className="h-9" />
       </div>
       <ul className="header__menu flex flex-row justify-center gap-x-5 font-medium list-none">
-        <Link to="/" className="text-white">
+        <Link to="/" className={`text-white ${activeMenuItem === 'Home' ? 'underline' : ''}`} onClick={() => handleMenuItemClick('Home')}>
           <li>Home</li>
         </Link>
-        <Link to="/myteam" className="text-white">
+        <Link to="/myteam" className={`text-white ${activeMenuItem === 'MyTeam' ? 'underline' : ''}`} onClick={() => handleMenuItemClick('MyTeam')}>
           <li>My Team</li>
         </Link>
-        <Link to="/exploreteam" className="text-white">
+        <Link to="/exploreteam" className={`text-white ${activeMenuItem === 'ExploreTeam' ? 'underline' : ''}`} onClick={() => handleMenuItemClick('ExploreTeam')}>
           <li>Explore Team</li>
         </Link>
-        <Link className="text-white">
+        <Link to="/profile" className={`text-white ${activeMenuItem === 'Profile' ? 'underline' : ''}`} onClick={() => handleMenuItemClick('Profile')}>
           <li>Profile</li>
         </Link>
       </ul>
@@ -29,7 +35,7 @@ function Header({ clickNotif, onLogout }) {
         <button className="mx-2 px-0 pb-0 pt-2" onClick={() => clickNotif()}>
           <IoNotificationsSharp />
         </button>
-        <button className="mx-2 px-0 pb-0 pt-2" onClick={onLogout}>
+        <button className="mx-2 px-0 pb-0 pt-2" onClick={logout}>
           <TbLogout />
         </button>
       </div>
