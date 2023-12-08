98b91e68da0b4503a7a15d4460cc25f58e89a6e8
RajendraDamar <rajendradamar70@gmail.com>
2023-12-08 07:12:19 +0700

    feat: add profile page animation

--- frontend/src/pages/MyProfilePage.jsx
+++ frontend/src/pages/MyProfilePage.jsx
@@ -6,6 +6,38 @@ import TeamList from '../components/TeamList';
 import Loading from '../components/Loading';
 
 function MyProfilePage({ user }) {
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.3,
+    };
+  
+    const handleIntersect = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          if (entry.target.classList.contains("animate-on-scroll-left")) {
+            entry.target.classList.add("slide-in-left");
+          } else if (entry.target.classList.contains("animate-on-scroll-right")) {
+            entry.target.classList.add("slide-in-right");
+          } else if (entry.target.classList.contains("animate-on-scroll")) {
+            entry.target.classList.add("fade-in");
+          } else if (entry.target.classList.contains("animate-on-scroll-pop")) {
+            entry.target.classList.add("pop");
+          }
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+  
+    const observer = new IntersectionObserver(handleIntersect, options);
+    const elements = document.querySelectorAll(".animate-on-scroll-left, .animate-on-scroll-right, .animate-on-scroll, .animate-on-scroll-pop");
+  
+    elements.forEach((element) => observer.observe(element));
+  
+    return () => observer.disconnect();
+  }, []);
+
   const [myTeams, setMyTeams] = useState('waiting');
   const [isLoading, setIsLoading] = useState(true);
 
@@ -36,12 +68,12 @@ function MyProfilePage({ user }) {
 
   return (
     <div className="profilePage bg-zinc-50 pt-[60px] pb-8 min-h-screen box-border flex flex-row gap-4 align-center justify-center">
-      <div className="profile  p-5">
+      <div className="profile  p-5 animate-on-scroll">
         <div className="max-w-[300px] mx-auto">
           <EditProfile user={user} />
         </div>
       </div>
-      <div className="teams-section w-[1330px] p-5">
+      <div className="teams-section w-[1330px] p-5 animate-on-scroll">
         <h2 className="text-indigo-950 text-4xl font-bold font-['Poppins'] leading-1">Your Teams</h2>
 
         {isLoading ? <Loading /> : <div className="mb-4">{myTeams.length > 0 ? <TeamList teams={myTeams} /> : <p>No Created Teams</p>}</div>}
